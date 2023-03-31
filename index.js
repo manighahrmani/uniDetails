cd8ca45fc9a0eb94fbc8fbc8c176ad9fca146029
manighahremani <mashapicasso@gmail.com>
2023-03-31 17:50:47 +0100

    Get also course titles

--- index.js
+++ index.js
@@ -6,17 +6,22 @@ const config = require('./config');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(config.url);
-  const universityList = await page.evaluate(() => {
+
+  const data = await page.evaluate(() => {
     const universityList = [];
+    const courseList = [];
     const headers = document.querySelectorAll('.course-title');
     headers.forEach((header) => {
-      const nextElement = header.nextElementSibling;
-      const universityName = nextElement.nextElementSibling;
+      const courseName = header.nextElementSibling;
+      const universityName = courseName.nextElementSibling;
       universityList.push(universityName.innerText);
+      courseList.push(header.innerText);
     });
-    return universityList;
+    const data = universityList.map((element, index) => [element, courseList[index]]);
+    return data;
   });
-  fs.appendFile(config.fileName, universityList.join(',\n'), (err) => {
+  console.log(data);
+  fs.appendFile(config.fileName, data.join(',\n'), (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
