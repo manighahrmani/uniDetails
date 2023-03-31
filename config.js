357fdf068ae986e67bec03c7f40030ef9f0e7d30
manighahremani <mashapicasso@gmail.com>
2023-03-31 16:51:29 +0100

    Separate constants to config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  url: "https://www.ucas.com/explore/courses?subject=Hospitality%2C%20leisure%2C%20and%20tourism&filterBy=all&studyYear=current&latLng=false&page=1",
+  fileName: 'university.csv'
+};
